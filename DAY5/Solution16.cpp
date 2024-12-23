e22fadd4f666627a35a84543160d10223e8876bb
mohit-thakre <mohitthakre010@gmail.com>
2024-12-23 19:58:25 +0530

    Solution 16

--- DAY5/Solution16.cpp
+++ DAY5/Solution16.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
 using namespace std;
 
+int fibonacci(int n)
+{
+
+    int a = 0;
+    int b = 1;
+    int c;
+
+    cout << a << " " << b << " ";
+    for (int i = 2; i <= n; i++)
+    {
+        c = a + b;
+        cout << c << " ";
+        a = b;
+        b = c;
+    }
+}
+
 int main()
 {
-    int n = 5;
-    if (n == 0)
-    {
-        cout << 0;
-    }
-    else if (n == 1)
-    {
-        cout << 0 << " " << 1 << endl;
-    }
-    else
-    {
-        int fib[n + 1];
-        fib[0] = 0;
-        fib[1] = 1;
-        for (int i = 2; i <= n; i++)
-        {
-            fib[i] = fib[i - 1] + fib[i - 2];
-        }
-        cout << "The Fibonacci Series up to " << n << "th term:" << endl;
-        for (int i = 0; i <= n; i++)
-        {
-            cout << fib[i] << " ";
-        }
-    }
+
+    fibonacci(6);
+    return 0;
 }
